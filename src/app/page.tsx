5f33b5eece23b3886687c6b38cb09de15af01254
Pavlo Lopatiuk <pashalopatuk@gmail.com>
2025-02-04 21:55:33 +0200

    Development: updated layout for different screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ArticlePreview from "@/components/article-preview";
 export default function Home() {
   return (
     <div>
-      <section className={'p-5 px-10 flex flex-col gap-10'}>
+      <section className={'p-5 px-10 flex flex-col gap-10 sm:grid grid-cols-2 lg:grid-cols-3'}>
         {articles.map(article => (
           <ArticlePreview
             previewImageUrl={article.previewImageUrl}
